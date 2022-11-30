9d02e9a2931a55abb081f35895b557d62d07a10b
Emily Cardwell <emily@emilycardwell.com>
2022-11-30 12:41:57 +0100

    11/30 add files

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import datetime
+import requests
+import pandas as pd
+
+'''
+# Chord Prediction Front
+'''
+columns = st.columns(3)
+a = columns[0].text_input("Input Chord 1", 'C')
+b = columns[0].text_input("Input Chord 2", 'A7')
+c = columns[0].text_input("Input Chord 3", 'F')
+
+d = a + "-" + b + "-" + c
+
+
+'''
+## Prediction
+'''
+@st.cache
+def call_api(chords):
+    url = 'https://final-project-api-zkfrzn26zq-ue.a.run.app/predict'
+    parameters = {'chords': chords}
+
+    response = requests.get(url, params=parameters)
+    return response.json()
+
+st.text(f'your chord progression: {d}...')
+st.text(call_api(d))
