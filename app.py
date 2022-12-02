3ffc0e3b952663722cd1a9ef35c6bf7ed214e087
Emily Cardwell <emily@emilycardwell.com>
2022-12-02 12:45:42 +0100

    changed some variables and readability

--- app.py
+++ app.py
@@ -9,19 +9,20 @@ a = columns[0].text_input("Input Chord 1", 'C')
 b = columns[0].text_input("Input Chord 2", 'A7')
 c = columns[0].text_input("Input Chord 3", 'F')
 
-d = [a, b, c]
+input_chords = [a, b, c]
 
 
 '''
 ## Prediction
 '''
 @st.cache
-def call_api(chords):
-    url = 'http://localhost:8000/'
-    parameters = {'chords': chords}
+def call_api(input_chords):
+    url = 'https://final-project-api-zkfrzn26zq-ue.a.run.app/predict'
+    parameters = {'chords': input_chords}
 
     response = requests.get(url, params=parameters)
     return response.json()
 
-st.text(f'your chord progression: {d}...')
-st.text(call_api(d))
+st.text(f'your chord progression: {input_chords}...')
+st.text("Your next chord:")
+st.text(call_api(input_chords))
