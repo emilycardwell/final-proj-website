597a2c88355ad12a87d82b84fe87756879c6c065
Emily Cardwell <emily@emilycardwell.com>
2022-12-06 17:45:57 +0100

    sizing

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def call_api(song, n_chords, randomness):
     return response
 
 if st.button('Get your prediction'):
-    new_text3 = '<p style="font-family:sans-serif; color:Black; font-size: 20px;">New chord progression:</p>'
+    new_text3 = '<p style="font-family:sans-serif; color:Black; font-size: 24px;">New chord progression:</p>'
     st.markdown(new_text3, unsafe_allow_html=True)
-    new_text4 = f'<p style="font-family:sans-serif; color:Black; font-size: 40px;">{call_api(song, n_chords, randomness)}</p>'
+    new_text4 = f'<p style="font-family:sans-serif; color:Black; font-size: 20px;">{call_api(song, n_chords, randomness)}</p>'
     st.markdown(new_text4, unsafe_allow_html=True)
