1449355adfe2ab0c3414fdb3b606f9bc58db641b
Emily Cardwell <emily@emilycardwell.com>
2022-12-06 18:41:01 +0100

    final attempt for today of chords

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import base64
 import numpy as np
-from cyp_defs import convert_chord_into_staff_and_midi_file, midi_to_wav
+from cyp_defs import convert_chord_into_staff_and_midi_file
 from pygame import mixer
 
 
@@ -65,25 +65,22 @@ if st.button('Get your prediction'):
     new_text4 = f'<p style="font-family:sans-serif; color:Black; font-size: 20px;">{call_api(song, n_chords, randomness)["predicted_chord"]}</p>'
     st.markdown(new_text4, unsafe_allow_html=True)
 
-    image_path_return , midi_path_return = convert_chord_into_staff_and_midi_file(call_api(song, n_chords, randomness)["predicted_chord"])
-    st.image(image_path_return)
-    mixer.init()
-    wav_file = midi_to_wav(midi_path_return)
-    music2 = open(wav_file,'wav')
+image_path_return , midi_path_return = convert_chord_into_staff_and_midi_file(call_api(song, n_chords, randomness)["predicted_chord"])
+st.image(image_path_return)
+mixer.init()
+music = st.file_uploader("Choose a song")
+try:
+    mixer.music.load(music)
+except Exception:
+    st.write("Please choose a song")
+if st.button("Play"):
+    mixer.music.play()
 
-    try:
-        mixer.music.load(music2)
-    except Exception:
-        st.write("Please choose a song")
+if st.button("Stop"):
+    mixer.music.stop()
 
-        if st.button("Play"):
-            mixer.music.play()
+if st.button("Resume"):
+    mixer.music.unpause()
 
-        if st.button("Stop"):
-            mixer.music.stop()
-
-        if st.button("Resume"):
-            mixer.music.unpause()
-
-        if st.button("Pause"):
-            mixer.music.pause()
+if st.button("Pause"):
+    mixer.music.pause()
