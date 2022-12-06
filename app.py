65e41d2f9a2d40c4d3d287798b48789db2aabff5
Emily Cardwell <emily@emilycardwell.com>
2022-12-06 17:54:33 +0100

    more formatting

--- app.py
+++ app.py
@@ -20,31 +20,30 @@ def add_bg_from_local(image_file):
     )
 add_bg_from_local('pics/vintage-2721099_1280.jpg')
 
+# websit title
 new_title = '<p style="font-family:sans-serif; color:Black; font-size: 42px;">Chord Progression Prediction</p>'
 st.markdown(new_title, unsafe_allow_html=True)
 
+# Input chords
 new_text = '<p style="font-family:sans-serif; color:Black; font-size: 20px;">Input 1 - 12 chords</p>'
 st.markdown(new_text, unsafe_allow_html=True)
-
 a = st.text_input("Input 1-12 chords", 'C,D,Em', label_visibility="collapsed")
 b = a.replace(' ', '')
-
 song = f'{b}'
 
+# select number of chords to predict
 new_text1 = '<p style="font-family:sans-serif; color:Black; font-size: 20px;">Select a number of predicted chords.</p>'
 st.markdown(new_text1, unsafe_allow_html=True)
-
 n_chords = st.selectbox('Select a number of predicted chords.', np.arange(1, 13, 1, 'int'), label_visibility="collapsed")
 
+# randomness slider
 new_text2 = '<p style="font-family:sans-serif; color:Black; font-size: 20px;">Pick the level of randomness (1 returns common chords, 10 returns more "interesting" chords).</p>'
 st.markdown(new_text2, unsafe_allow_html=True)
-
 randomness = st.slider('Pick the level of randomness.', 1, 10, 3, label_visibility="collapsed")
-
-
 '''
 ---
 '''
+# call API
 @st.cache(suppress_st_warning=True)
 def call_api(song, n_chords, randomness):
     url = 'https://chords-progression-prediction-1-zkfrzn26zq-ew.a.run.app/predict'
@@ -58,7 +57,7 @@ def call_api(song, n_chords, randomness):
     return response
 
 if st.button('Get your prediction'):
-    new_text3 = '<p style="font-family:sans-serif; color:Black; font-size: 24px;">New chord progression:</p>'
+    new_text3 = '<p style="font-family:sans-serif; color:Red; font-size: 24px;">New chord progression:</p>'
     st.markdown(new_text3, unsafe_allow_html=True)
     new_text4 = f'<p style="font-family:sans-serif; color:Black; font-size: 20px;">{call_api(song, n_chords, randomness)["predicted_chord"]}</p>'
     st.markdown(new_text4, unsafe_allow_html=True)
