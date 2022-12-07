11e44296881e1b1fe93283a2419e2ae4cb74e18c
Emily Cardwell <emily@emilycardwell.com>
2022-12-07 11:35:46 +0100

    trying staff again

--- app.py
+++ app.py
@@ -65,22 +65,22 @@ if st.button('Get your prediction'):
     new_text4 = f'<p style="font-family:sans-serif; color:Black; font-size: 20px;">{call_api(song, n_chords, randomness)["predicted_chord"]}</p>'
     st.markdown(new_text4, unsafe_allow_html=True)
 
-image_path_return , midi_path_return = convert_chord_into_staff_and_midi_file(call_api(song, n_chords, randomness)["predicted_chord"])
-st.image(image_path_return)
-mixer.init()
-music = st.file_uploader("Choose a song")
-try:
-    mixer.music.load(music)
-except Exception:
-    st.write("Please choose a song")
-if st.button("Play"):
-    mixer.music.play()
+    image_path_return , midi_path_return = convert_chord_into_staff_and_midi_file(call_api(song, n_chords, randomness)["predicted_chord"])
+    st.image(image_path_return)
+    mixer.init()
+    music = st.file_uploader("Choose a song")
+    try:
+        mixer.music.load(music)
+    except Exception:
+        st.write("Please choose a song")
+    if st.button("Play"):
+        mixer.music.play()
 
-if st.button("Stop"):
-    mixer.music.stop()
+    if st.button("Stop"):
+        mixer.music.stop()
 
-if st.button("Resume"):
-    mixer.music.unpause()
+    if st.button("Resume"):
+        mixer.music.unpause()
 
-if st.button("Pause"):
-    mixer.music.pause()
+    if st.button("Pause"):
+        mixer.music.pause()
